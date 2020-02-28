3b069ff4d839e4ee4bd4d888ee9d6b023e4f2b8e
laurence.ede <laurence.ede@gmail.com>
2020-02-28 07:32:05 +0000

    Updated port for materials

--- Materials/data/import.sh
+++ Materials/data/import.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-mongoimport --db materials --collection units --file ${DIR}/units.json --drop --username root --password example --authenticationDatabase admin
-mongoimport --db materials --collection materials --file ${DIR}/materials.json --drop --username root --password example --authenticationDatabase admin
+PORT=27018
+mongoimport --port=${PORT} --db materials --collection units --file ${DIR}/units.json --drop --username root --password example --authenticationDatabase admin
+mongoimport --port=${PORT} --db materials --collection materials --file ${DIR}/materials.json --drop --username root --password example --authenticationDatabase admin
